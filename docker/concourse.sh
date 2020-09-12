469f7959c242e19533b1d2a892fe570beb210062
Dmitrii Sosedov <dsosedov@vmware.com>
2020-09-11 23:00:14 -0400

    Update concourse configuration

--- docker/concourse.sh
+++ docker/concourse.sh
@@ -12,9 +12,9 @@ docker run \
     -e CONCOURSE_POSTGRES_PASSWORD=postgresadmin \
     -e CONCOURSE_POSTGRES_DATABASE=concourse \
     -e CONCOURSE_EXTERNAL_URL=http://localhost:8181 \
-    -e CONCOURSE_ADD_LOCAL_USER=test:test \
-    -e CONCOURSE_MAIN_TEAM_LOCAL_USER=test \
-    -e CONCOURSE_WORKER_BAGGAGECLAIM_DRIVER=overlay \
+    -e CONCOURSE_ADD_LOCAL_USER=admin:Admin!,ds:Ds! \
+    -e CONCOURSE_MAIN_TEAM_LOCAL_USER=admin \
+    -e CONCOURSE_WORKER_BAGGAGECLAIM_DRIVER=naive \
     -e CONCOURSE_CLIENT_SECRET=Y29uY291cnNlLXdlYgo= \
     -e CONCOURSE_TSA_CLIENT_SECRET=Y29uY291cnNlLXdvcmtlcgo= \
     concourse/concourse \
